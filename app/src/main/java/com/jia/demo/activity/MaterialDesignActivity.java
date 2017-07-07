37e10c8a0e04f28b9bcf400ac1a09a4b075ab5fa
shuaijia <819418850@qq.com>
2017-07-07 16:05:34 +0800

    jia:质感设计界面，越界回弹

--- app/src/main/java/com/jia/demo/activity/MaterialDesignActivity.java
+++ app/src/main/java/com/jia/demo/activity/MaterialDesignActivity.java
@@ -1,7 +1,12 @@
 package com.jia.demo.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.jia.demo.R;
 
@@ -10,13 +15,20 @@ import com.jia.demo.R;
  * Created by jia on 2017/04/10
  * 人之所以能，是相信能
  */
-public class MaterialDesignActivity extends Activity{
+public class MaterialDesignActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_material_design);
 
+        //给页面设置工具栏
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //设置工具栏标题
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolbarLayout);
+        collapsingToolbar.setTitle("下拉回弹");
     }
 }

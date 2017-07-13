d3d939bd8702addea4e02de1c6055ad19f83b77e
shuaijia <819418850@qq.com>
2017-07-13 09:45:43 +0800

    jia:沉浸式状态栏

--- app/src/main/java/com/jia/demo/activity/StatusBarActivity.java
+++ app/src/main/java/com/jia/demo/activity/StatusBarActivity.java
@@ -1,22 +1,16 @@
 package com.jia.demo.activity;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
 
 import com.jia.demo.R;
 
 /**
- * Describtion:
+ * Describtion:沉浸式状态栏
  * Created by jia on 2017/7/11.
  * 人之所以能，是相信能
  */
 public class StatusBarActivity extends Activity {
-
-    private TextView tv_status_theme;
-    private TextView tv_status_view_hight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

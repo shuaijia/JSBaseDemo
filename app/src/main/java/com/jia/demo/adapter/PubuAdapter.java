c56136bb2696c472ed2ef9fb9c27c9c72697080a
shuajia <819418850@qq.com>
2017-07-31 16:16:52 +0800

    jia:inflate方法参数

--- app/src/main/java/com/jia/demo/adapter/PubuAdapter.java
+++ app/src/main/java/com/jia/demo/adapter/PubuAdapter.java
@@ -17,18 +17,18 @@ import java.util.List;
  * Created by jia on 2017/6/22.
  * 人之所以能，是相信能
  */
-public class PubuAdapter extends RecyclerView.Adapter<PubuAdapter.MasonryView>{
+public class PubuAdapter extends RecyclerView.Adapter<PubuAdapter.MasonryView> {
     private List<String> products;
-    Context context;
+    private Context context;
 
-    public PubuAdapter(List<String> list,Context context) {
-        products=list;
-        this.context=context;
+    public PubuAdapter(List<String> list, Context context) {
+        products = list;
+        this.context = context;
     }
 
     @Override
     public MasonryView onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View view= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_recyclerview, null);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_recyclerview, viewGroup, false);
         return new MasonryView(view);
     }
 
@@ -45,13 +45,13 @@ public class PubuAdapter extends RecyclerView.Adapter<PubuAdapter.MasonryView>{
         return products.size();
     }
 
-    public static class MasonryView extends  RecyclerView.ViewHolder{
+    public static class MasonryView extends RecyclerView.ViewHolder {
 
         ImageView imageView;
 
-        public MasonryView(View itemView){
+        public MasonryView(View itemView) {
             super(itemView);
-            imageView= (ImageView) itemView.findViewById(R.id.iv_recycler_img );
+            imageView = (ImageView) itemView.findViewById(R.id.iv_recycler_img);
         }
 
     }

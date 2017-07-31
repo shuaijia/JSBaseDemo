2a6df291bceb4bdba9937217a65719d499e45524
shuajia <819418850@qq.com>
2017-07-31 11:26:22 +0800

    jia:将瀑布流界面改为inflate方法测试界面

--- app/src/main/java/com/jia/demo/activity/RecyclerViewActivity.java
+++ app/src/main/java/com/jia/demo/activity/RecyclerViewActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.AttributeSet;
@@ -31,17 +32,8 @@ public class RecyclerViewActivity extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recyclerview);
         rv_pubu= (RecyclerView) findViewById(R.id.rv_pubu);
-        final StaggeredGridLayoutManager ll=new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
-        ll.setGapStrategy(StaggeredGridLayoutManager.GAP_HANDLING_NONE);
+        final LinearLayoutManager ll=new LinearLayoutManager(RecyclerViewActivity.this);
         rv_pubu.setLayoutManager(ll);
-
-        rv_pubu.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                ll.invalidateSpanAssignments();
-            }
-        });
 
 
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887285&di=ae381e53991dc0c9303864ddcac3f3b8&imgtype=0&src=http%3A%2F%2Fcdnq.duitang.com%2Fuploads%2Fitem%2F201409%2F18%2F20140918231127_LZQjr.png");
@@ -52,7 +44,6 @@ public class RecyclerViewActivity extends Activity{
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887285&di=2dabbc7b01140d9e9a97bedd8a2a2fd8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Ffd039245d688d43f4c195ed5771ed21b0ef43b61.jpg");
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887283&di=f2210b8eb94211d055928c32553b7b2d&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2012%2F0602%2Fyt%2F18%2F73869f30446c7978916445105b015d97.jpg");
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887285&di=2dabbc7b01140d9e9a97bedd8a2a2fd8&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2Ffd039245d688d43f4c195ed5771ed21b0ef43b61.jpg");
-//        imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887284&di=e2b30c00bd0e449932b33c535e0dcc5e&imgtype=0&src=http%3A%2F%2Fh8.86.cc%2Fwalls%2F20160801%2F1440x900_747fcda589b922f.jpg");
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887283&di=f2210b8eb94211d055928c32553b7b2d&imgtype=0&src=http%3A%2F%2Ftupian.enterdesk.com%2F2012%2F0602%2Fyt%2F18%2F73869f30446c7978916445105b015d97.jpg");
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887283&di=1049938e2e341cfeb80475217c2fa580&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201508%2F28%2F20150828215515_wtuLm.jpeg");
         imgs.add("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1498110887283&di=1049938e2e341cfeb80475217c2fa580&imgtype=0&src=http%3A%2F%2Fimg4.duitang.com%2Fuploads%2Fitem%2F201508%2F28%2F20150828215515_wtuLm.jpeg");

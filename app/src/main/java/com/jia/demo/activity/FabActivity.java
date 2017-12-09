ae8a13cb2cc8560e70f84526005bb1c8d0669108
shuajia <819418850@qq.com>
2017-12-09 09:41:33 +0800

    jia：fab

---
+++ app/src/main/java/com/jia/demo/activity/FabActivity.java
@@ -0,0 +1,18 @@
+package com.jia.demo.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jia.demo.R;
+
+/**
+ * 浮动按钮界面
+ */
+public class FabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fab);
+    }
+}

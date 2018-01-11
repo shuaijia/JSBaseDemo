396583e03624d7f23b4ad9a18120aa8a7f189245
shuaijia <819418850@qq.com>
2018-01-11 09:52:40 +0800

    jia：快速滑动   glide暂停加载

--- app/src/main/java/com/jia/demo/activity/ParallaxActivity.java
+++ app/src/main/java/com/jia/demo/activity/ParallaxActivity.java
@@ -2,7 +2,10 @@ package com.jia.demo.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.AbsListView;
 
+import com.bumptech.glide.Glide;
 import com.jia.demo.R;
 import com.jia.demo.adapter.ParallaxAdapter;
 import com.jia.demo.view.JsParallaxRecyclerView;
@@ -25,5 +28,37 @@ public class ParallaxActivity extends Activity {
         rv_parallax= (JsParallaxRecyclerView) findViewById(R.id.rv_parallax);
 
         rv_parallax.setAdapter(new ParallaxAdapter(ParallaxActivity.this));
+
+        rv_parallax.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                switch (newState){
+                    // 停止滑动
+                    case AbsListView.OnScrollListener.SCROLL_STATE_IDLE:
+
+                        Glide.with(ParallaxActivity.this).resumeRequests();
+
+                        break;
+                    // 快速滑动中
+                    case AbsListView.OnScrollListener.SCROLL_STATE_FLING:
+
+                        Glide.with(ParallaxActivity.this).pauseRequests();
+
+                        break;
+                    // 手指触摸滑动开始
+                    case AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
+
+
+                        break;
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
     }
 }
--- app/src/main/java/com/jia/demo/adapter/ParallaxAdapter.java
+++ app/src/main/java/com/jia/demo/adapter/ParallaxAdapter.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.jia.demo.R;
 
 /**
@@ -28,17 +30,25 @@ public class ParallaxAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        Glide.with(context)
+                .load("https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1515645221735&di=53676e8ea61db1a81386ddda2713c1ab&imgtype=0&src=http%3A%2F%2Fimgsrc.baidu.com%2Fimgad%2Fpic%2Fitem%2F7c1ed21b0ef41bd5af9a14e85bda81cb38db3de4.jpg")
+                .centerCrop()
+                .into(((Holder)holder).iv);
     }
 
     @Override
     public int getItemCount() {
-        return 50;
+        return 100;
     }
 
     private class Holder extends RecyclerView.ViewHolder {
 
+        public ImageView iv;
+
         Holder(View itemView) {
             super(itemView);
+            iv= (ImageView) itemView.findViewById(R.id.iv);
         }
 
     }

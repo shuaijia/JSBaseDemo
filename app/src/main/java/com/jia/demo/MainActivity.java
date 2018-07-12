7ad88ca26dcbef45c260140d285e953b11de14a3
jiashuai <368419jiashuai>
2018-07-12 15:29:58 +0800

    jia: item数目

--- app/src/main/java/com/jia/demo/MainActivity.java
+++ app/src/main/java/com/jia/demo/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends Activity {
 
 //      ===================BaseAdapter测试=====================================
         List<Login> list = new ArrayList<>();
-        for (int i = 0; i < 80; i++) {
+        for (int i = 0; i < 70; i++) {
             Login login = new Login();
             switch (i) {
                 case 0:
